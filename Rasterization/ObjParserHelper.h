f5241c8fa30f69cb7c1b4bcee1ebf47a1d29b1bb
goliver5 <oliver_msn94@hotmail.com>
2022-08-05 01:14:49 +0200

    changes

--- Rasterization/ObjParserHelper.h
+++ Rasterization/ObjParserHelper.h
@@ -9,10 +9,7 @@ struct objectStruct
 	std::vector<float> uvs;
 	std::vector<VertexData> vertexForIndex;
 	std::vector<int> vertexSubMeshCounter;
+	std::vector<int> indices
 };
 
 void ObjParserHelper(std::vector<objectStruct>& objectInformation);
-
-
-std::vector<float>& vertices, std::vector<float>& normals, std::vector<float>& uvs, std::vector<VertexData>& vertexForIndex,
-std::vector<int>& vertexSubMeshCounter, std::vector<int>& indices
